612c937731b75993fd6e1af4cc35bb05eed3dfd4
thomaslb <lagaba@live.unc.edu>
2024-07-22 19:54:14 -0400

    Enable access to the backend services and fix routing to allow refreshing of the angular page

--- frontend/src/app/ai.service.ts
+++ frontend/src/app/ai.service.ts
@@ -14,7 +14,7 @@ import { map, shareReplay } from 'rxjs/operators';
 //     Use RxJS to Cache API Responses:
 
 export class AiService {
-  private apiUrl = 'https://185.211.4.179:3000/api';
+  private apiUrl = 'https://teach.webexpansions.com/api';
 
   constructor(private http: HttpClient) { }
 
